55b9a50d03d6aaaaf0508c57390c44af330ab28c
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-22 20:39:06 -0500

    Added useState in the dashboard.jsx file.

---
+++ src/container/Dashboard/dashboard.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import React, {useState} from 'react';
+
+const dashboard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default dashboard;
