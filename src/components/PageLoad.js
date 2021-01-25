d2e1d0feada33f2f64a24a63a7db27428cc9afab
Valkimani <66477953+Valkimani@users.noreply.github.com>
2021-01-25 08:53:19 -0500

    Created a Page load.js file in the components. Added a class and render to return the page content.

---
+++ src/components/PageLoad.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PageLoad extends React.Component {
+    render (){
+        return(
+             <div>
+            
+        </div>
+        
+    );
+        }
+};
+
+export default PageLoad;
